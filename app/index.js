ed4c388bbcef9d8b150f35988f42f1cee978886d
zhi.chen <zhi.chen@ucarinc.com>
2017-11-14 22:58:02 +0800

    手动搭建react环境

---
+++ app/index.js
@@ -0,0 +1,6 @@
+function component(){
+    var element=document.createElement('div');
+    element.innerHTML=('Hello Jinnjjjyyuuuj');
+    return element;
+}
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports={
+    entry:'./app/index.js',
+    output:{
+        filename:'index.js',
+        path:path.resolve(__dirname,'dist'),
+        publicPath:'temp/'
+    },
+    devServer:{
+        contentBase:'./',
+        host:'localhost',
+        compress:true,
+        port:1717
+    }
+    
+}
